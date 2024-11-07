c9196196a4ec1aca98a549262912a4bca55b9169
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2024-11-07 13:18:00 -0500

    practica

---
+++ src/main/java/zamira/estudiante/zamirashiguango2024/ZamiraShiguango2024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package zamira.estudiante.zamirashiguango2024;
+
+/**
+ *
+ * @author user
+ */
+public class ZamiraShiguango2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
